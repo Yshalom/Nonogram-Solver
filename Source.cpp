eb754d2cacc4373775175f4a8ae918a0c58fb532
Yshalom <109998553+Yshalom@users.noreply.github.com>
2024-04-28 00:44:51 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -5,30 +5,28 @@
 using namespace std;
 
 bool play = true, play_input = true;
-int size_bord, move_panel;
+int size_board, move_panel;
 
 LRESULT CALLBACK WindowProc_input(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam);
 LRESULT CALLBACK WindowProc_main(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam);
-void input_panel();
-void main_panel();
-void updateBord_main();
-bool** solve_main();
-Mode3 solve_rec_main(Mode3);
+void input_panel(HINSTANCE);
+void main_panel(HINSTANCE);
+void updateBoard();
+bool** solve_board();
+Mode3 solve_rec(Mode3);
 vector<int> ToVector(char[]);
 
-int main()
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
 {
-	FreeConsole();
-
 	while (true)
 	{
-		input_panel();
+		input_panel(hInstance);
 		if (!play)
 			return 0;
 		play_input = true;
 		play = true;
 
-		main_panel();
+		main_panel(hInstance);
 		play = true;
 	}
 
@@ -36,19 +34,19 @@ int main()
 }
 
 HWND button_input, size_input, move_input;
-void input_panel()
+void input_panel(HINSTANCE hInstance)
 {
-	HWND hwnd = CreateWindowA("Static", "Nonogram", WS_VISIBLE | WS_OVERLAPPEDWINDOW, 500, 400, 350, 250, NULL, NULL, NULL, NULL);
+	HWND hwnd = CreateWindowA("Static", "Nonogram", WS_VISIBLE | WS_OVERLAPPEDWINDOW, 500, 400, 350, 250, NULL, NULL, hInstance, NULL);
 	SetWindowLongPtr(hwnd, -4, (LONG)WindowProc_input);
 	HBRUSH brush = CreateSolidBrush(RGB(235, 235, 235));
 	SetClassLongPtr(hwnd, GCLP_HBRBACKGROUND, (LONG_PTR)brush);
 	InvalidateRect(hwnd, NULL, TRUE);
 
-	CreateWindowA("Static", "Write the size of nonogram bord:", WS_VISIBLE | WS_CHILD, 30, 50, 215, 20, hwnd, NULL, NULL, NULL);
-	size_input = CreateWindowA("Edit", "", WS_VISIBLE | WS_CHILD | ES_NUMBER, 250, 50, 24, 20, hwnd, NULL, NULL, NULL);
-	CreateWindowA("Static", "Write the size of nonogram Input:", WS_VISIBLE | WS_CHILD, 30, 100, 225, 20, hwnd, NULL, NULL, NULL);
-	move_input = CreateWindowA("Edit", "", WS_VISIBLE | WS_CHILD | ES_NUMBER, 260, 100, 24, 20, hwnd, NULL, NULL, NULL);
-	button_input = CreateWindowA("Button", "OK", WS_VISIBLE | WS_CHILD | ES_NUMBER, 125, 150, 50, 30, hwnd, NULL, NULL, NULL);
+	CreateWindowA("Static", "Write the size of nonogram board:", WS_VISIBLE | WS_CHILD, 30, 50, 230, 20, hwnd, NULL, hInstance, NULL);
+	size_input = CreateWindowA("Edit", "", WS_VISIBLE | WS_CHILD | ES_NUMBER, 260, 50, 24, 20, hwnd, NULL, hInstance, NULL);
+	CreateWindowA("Static", "Write the size of nonogram Input:", WS_VISIBLE | WS_CHILD, 30, 100, 225, 20, hwnd, NULL, hInstance, NULL);
+	move_input = CreateWindowA("Edit", "", WS_VISIBLE | WS_CHILD | ES_NUMBER, 260, 100, 24, 20, hwnd, NULL, hInstance, NULL);
+	button_input = CreateWindowA("Button", "OK", WS_VISIBLE | WS_CHILD | ES_NUMBER, 125, 150, 50, 30, hwnd, NULL, hInstance, NULL);
 
 
 	MSG msg;
@@ -81,15 +79,15 @@ LRESULT CALLBACK WindowProc_input(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wP
 		{
 			char* a = new char[4];
 			GetWindowTextA(size_input, a, 4);
-			size_bord = 0;
+			size_board = 0;
 			for (int i = 0; i < 4 && a[i]; i++)
 			{
-				size_bord *= 10;
-				size_bord += a[i] - '0';
+				size_board *= 10;
+				size_board += a[i] - '0';
 			}
 
-			if (size_bord == 0)
-				size_bord = 10;
+			if (size_board == 0)
+				size_board = 10;
 			
 			GetWindowTextA(move_input, a, 4);
 			move_panel = 0;
@@ -100,7 +98,7 @@ LRESULT CALLBACK WindowProc_input(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wP
 			}
 
 			if (move_panel == 0)
-				move_panel = 2 * size_bord - 20;
+				move_panel = 2 * size_board - 20;
 
 			play_input = false;
 			delete[] a;
@@ -112,57 +110,57 @@ LRESULT CALLBACK WindowProc_input(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wP
 }
 
 HWND hwnd, hImageView_main, button_main, Stamp;
-bool** bord_main = NULL;
+bool** board_main = NULL;
 vector<HWND> X_input_main, Y_input_main;
-void main_panel()
+void main_panel(HINSTANCE hInstance)
 {
 	RECT r;
 	GetWindowRect(GetDesktopWindow(), &r);
-	int x = (r.right - size_bord * 20 - 160) / 2,
-		y = (r.bottom - size_bord * 20 - 195) / 2;
+	int x = (r.right - size_board * 20 - 160) / 2,
+		y = (r.bottom - size_board * 20 - 195) / 2;
 
-	hwnd = CreateWindowA("Static", "Nonogram", WS_VISIBLE | WS_OVERLAPPEDWINDOW, x, y, size_bord * 20 + 160 + move_panel, size_bord * 20 + 195 + move_panel, NULL, NULL, NULL, NULL);
+	hwnd = CreateWindowA("Static", "Nonogram", WS_VISIBLE | WS_OVERLAPPEDWINDOW, x, y, size_board * 20 + 160 + move_panel, size_board * 20 + 195 + move_panel, NULL, NULL, hInstance, NULL);
 	SetWindowLongPtr(hwnd, -4, (LONG)WindowProc_main);
 	HBRUSH brush = CreateSolidBrush(RGB(235, 235, 235));
 	SetClassLongPtr(hwnd, GCLP_HBRBACKGROUND, (LONG_PTR)brush);
 	InvalidateRect(hwnd, NULL, TRUE);
 
-	hImageView_main = CreateWindowEx(NULL, L"STATIC", NULL, SS_BITMAP | WS_VISIBLE | WS_CHILD, 70 + move_panel, 95 + move_panel, 0, 0, hwnd, NULL, NULL, NULL);
-	button_main = CreateWindowA("Button", "update the bord", WS_VISIBLE | WS_CHILD, size_bord * 10 + move_panel, size_bord * 20 + 113 + move_panel, 130, 30, hwnd, NULL, NULL, NULL);
-	Stamp = CreateWindowA("Static", "ιφηχ ωμεν", WS_VISIBLE | WS_CHILD, 10, size_bord * 20 + 133 + move_panel, 70, 20, hwnd, NULL, NULL, NULL);
-	X_input_main.resize(size_bord);
-	Y_input_main.resize(size_bord);
-	for (int i = 0; i < size_bord; i++)
+	hImageView_main = CreateWindowEx(NULL, L"STATIC", NULL, SS_BITMAP | WS_VISIBLE | WS_CHILD, 70 + move_panel, 95 + move_panel, 0, 0, hwnd, NULL, hInstance, NULL);
+	button_main = CreateWindowA("Button", "update the board", WS_VISIBLE | WS_CHILD, size_board * 10 + move_panel, size_board * 20 + 113 + move_panel, 130, 30, hwnd, NULL, hInstance, NULL);
+	Stamp = CreateWindowA("Static", "Χ™Χ¦Χ—Χ§ Χ©ΧΧ•Χ", WS_VISIBLE | WS_CHILD, 10, size_board * 20 + 133 + move_panel, 70, 20, hwnd, NULL, hInstance, NULL);
+	X_input_main.resize(size_board);
+	Y_input_main.resize(size_board);
+	for (int i = 0; i < size_board; i++)
 	{
-		X_input_main[i] = CreateWindowA("Edit", "", WS_VISIBLE | WS_CHILD, 10, 96 + 20 * i + move_panel, 58 + move_panel, 18, hwnd, NULL, NULL, NULL);
-		Y_input_main[i] = CreateWindowA("Edit", "", WS_VISIBLE | WS_CHILD | ES_MULTILINE, 71 + 20 * i + move_panel, 10, 18, 83 + move_panel, hwnd, NULL, NULL, NULL);
+		X_input_main[i] = CreateWindowA("Edit", "", WS_VISIBLE | WS_CHILD, 10, 96 + 20 * i + move_panel, 58 + move_panel, 18, hwnd, NULL, hInstance, NULL);
+		Y_input_main[i] = CreateWindowA("Edit", "", WS_VISIBLE | WS_CHILD | ES_MULTILINE, 71 + 20 * i + move_panel, 10, 18, 83 + move_panel, hwnd, NULL, hInstance, NULL);
 	}
 	
 	HDC hdcImage = GetDC(hImageView_main);
-	for (int i = 0; i < size_bord * 20; i++)
-		for (int j = 0; j < size_bord * 20; j++)
+	for (int i = 0; i < size_board * 20; i++)
+		for (int j = 0; j < size_board * 20; j++)
 		{
 			if (i % 20 == 0 || i % 20 == 19 || j % 20 == 0 || j % 20 == 19)
 				SetPixelV(hdcImage, i, j, RGB(127, 127, 127));
 			else
 				SetPixelV(hdcImage, j, i, RGB(255, 255, 255));
 		}
-	for (int i = 0; i < size_bord * 20; i++)
+	for (int i = 0; i < size_board * 20; i++)
 	{
 		SetPixelV(hdcImage, i, 1, RGB(127, 127, 127));
 		SetPixelV(hdcImage, i, 2, RGB(127, 127, 127));
 		SetPixelV(hdcImage, 1, i, RGB(127, 127, 127));
 		SetPixelV(hdcImage, 2, i, RGB(127, 127, 127));
-		SetPixelV(hdcImage, i, size_bord * 20 - 2, RGB(127, 127, 127));
-		SetPixelV(hdcImage, i, size_bord * 20 - 3, RGB(127, 127, 127));
-		SetPixelV(hdcImage, size_bord * 20 - 2, i, RGB(127, 127, 127));
-		SetPixelV(hdcImage, size_bord * 20 - 3, i, RGB(127, 127, 127));
+		SetPixelV(hdcImage, i, size_board * 20 - 2, RGB(127, 127, 127));
+		SetPixelV(hdcImage, i, size_board * 20 - 3, RGB(127, 127, 127));
+		SetPixelV(hdcImage, size_board * 20 - 2, i, RGB(127, 127, 127));
+		SetPixelV(hdcImage, size_board * 20 - 3, i, RGB(127, 127, 127));
 	}
-	if (size_bord % 5 == 0)
+	if (size_board % 5 == 0)
 	{
-		for (int i = 5; i < size_bord; i += 5)
+		for (int i = 5; i < size_board; i += 5)
 		{
-			for (int j = 0; j < size_bord * 20; j++)
+			for (int j = 0; j < size_board * 20; j++)
 			{
 				SetPixelV(hdcImage, i * 20 + 1, j, RGB(127, 127, 127));
 				SetPixelV(hdcImage, j, i * 20 + 1, RGB(127, 127, 127));
@@ -192,7 +190,7 @@ LRESULT CALLBACK WindowProc_main(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wPa
 
 	case WM_COMMAND:
 		if ((HWND)lParam == button_main)
-			updateBord_main();
+			updateBoard();
 		break;
 	case WM_CTLCOLORSTATIC:
 	{
@@ -204,12 +202,12 @@ LRESULT CALLBACK WindowProc_main(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wPa
 	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
-void updateBord_main()
+void updateBoard()
 {
 	HDC hdcImage = GetDC(hImageView_main);
 
-	for (int i = size_bord * 20 - 1; i >= 0; i--)
-		for (int j = size_bord * 20 - 1; j >= 0; j--)
+	for (int i = size_board * 20 - 1; i >= 0; i--)
+		for (int j = size_board * 20 - 1; j >= 0; j--)
 		{
 			if (i % 20 == 0 || i % 20 == 19 || j % 20 == 0 || j % 20 == 19)
 				SetPixelV(hdcImage, i, j, RGB(235, 235, 235));
@@ -237,8 +235,8 @@ void updateBord_main()
 		{1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1},
 		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
 		};
-	for (int i = 0; i < size_bord * 20; i++)
-		for (int j = 0; j < size_bord * 20; j++)
+	for (int i = 0; i < size_board * 20; i++)
+		for (int j = 0; j < size_board * 20; j++)
 		{
 			if (i % 20 == 0 || i % 20 == 19 || j % 20 == 0 || j % 20 == 19)
 				SetPixelV(hdcImage, i, j, RGB(127, 127, 127));
@@ -247,10 +245,10 @@ void updateBord_main()
 		}
 	
 	try {
-		bool** a = solve_main();
+		bool** a = solve_board();
 
-		for (int i = size_bord * 20 - 1; i >= 0; i--)
-			for (int j = size_bord * 20 - 1; j >= 0; j--)
+		for (int i = size_board * 20 - 1; i >= 0; i--)
+			for (int j = size_board * 20 - 1; j >= 0; j--)
 			{
 				if (i % 20 == 0 || i % 20 == 19 || j % 20 == 0 || j % 20 == 19)
 					SetPixelV(hdcImage, i, j, RGB(235, 235, 235));
@@ -278,8 +276,8 @@ void updateBord_main()
 			{0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0},
 			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 		};
-		for (int i = 0; i < size_bord * 20; i++)
-			for (int j = 0; j < size_bord * 20; j++)
+		for (int i = 0; i < size_board * 20; i++)
+			for (int j = 0; j < size_board * 20; j++)
 			{
 				if (i % 20 == 0 || i % 20 == 19 || j % 20 == 0 || j % 20 == 19)
 					SetPixelV(hdcImage, i, j, RGB(127, 127, 127));
@@ -287,14 +285,14 @@ void updateBord_main()
 					SetPixelV(hdcImage, j, i, (a[i / 20][j / 20] ? RGB(0, 0, 0) : (X[i % 20 - 1][j % 20 - 1] ? RGB(0, 0, 0) : RGB(255, 255, 255))));
 			}
 
-		for (int i = 0; i < size_bord; i++)
+		for (int i = 0; i < size_board; i++)
 			delete[] a[i];
 		delete a;
 	}
 	catch (const char* c)
 	{
-		for (int i = size_bord * 20 - 1; i >= 0; i--)
-			for (int j = size_bord * 20 - 1; j >= 0; j--)
+		for (int i = size_board * 20 - 1; i >= 0; i--)
+			for (int j = size_board * 20 - 1; j >= 0; j--)
 			{
 				if (i % 20 == 0 || i % 20 == 19 || j % 20 == 0 || j % 20 == 19)
 					SetPixelV(hdcImage, i, j, RGB(235, 235, 235));
@@ -302,8 +300,8 @@ void updateBord_main()
 					SetPixelV(hdcImage, j, i, RGB(235, 235, 235));
 			}
 
-		for (int i = 0; i < size_bord * 20; i++)
-			for (int j = 0; j < size_bord * 20; j++)
+		for (int i = 0; i < size_board * 20; i++)
+			for (int j = 0; j < size_board * 20; j++)
 			{
 				if (i % 20 == 0 || i % 20 == 19 || j % 20 == 0 || j % 20 == 19)
 					SetPixelV(hdcImage, i, j, RGB(127, 127, 127));
@@ -311,22 +309,22 @@ void updateBord_main()
 					SetPixelV(hdcImage, j, i, RGB(255, 255, 255));
 			}
 	}
-	for (int i = 0; i < size_bord * 20; i++)
+	for (int i = 0; i < size_board * 20; i++)
 	{
 		SetPixelV(hdcImage, i, 1, RGB(127, 127, 127));
 		SetPixelV(hdcImage, i, 2, RGB(127, 127, 127));
 		SetPixelV(hdcImage, 1, i, RGB(127, 127, 127));
 		SetPixelV(hdcImage, 2, i, RGB(127, 127, 127));
-		SetPixelV(hdcImage, i, size_bord * 20 - 2, RGB(127, 127, 127));
-		SetPixelV(hdcImage, i, size_bord * 20 - 3, RGB(127, 127, 127));
-		SetPixelV(hdcImage, size_bord * 20 - 2, i, RGB(127, 127, 127));
-		SetPixelV(hdcImage, size_bord * 20 - 3, i, RGB(127, 127, 127));
+		SetPixelV(hdcImage, i, size_board * 20 - 2, RGB(127, 127, 127));
+		SetPixelV(hdcImage, i, size_board * 20 - 3, RGB(127, 127, 127));
+		SetPixelV(hdcImage, size_board * 20 - 2, i, RGB(127, 127, 127));
+		SetPixelV(hdcImage, size_board * 20 - 3, i, RGB(127, 127, 127));
 	}
-	if (size_bord % 5 == 0)
+	if (size_board % 5 == 0)
 	{
-		for (int i = 5; i < size_bord; i += 5)
+		for (int i = 5; i < size_board; i += 5)
 		{
-			for (int j = 0; j < size_bord * 20; j++)
+			for (int j = 0; j < size_board * 20; j++)
 			{
 				SetPixelV(hdcImage, i * 20 + 1, j, RGB(127, 127, 127));
 				SetPixelV(hdcImage, j, i * 20 + 1, RGB(127, 127, 127));
@@ -337,12 +335,12 @@ void updateBord_main()
 	DeleteDC(hdcImage);
 }
 
-bool** solve_main()
+bool** solve_board()
 {
-	Mode3::size = size_bord;
+	Mode3::size = size_board;
 	Mode3::right.clear();
 	Mode3::top.clear();
-	for (int i = 0; i < size_bord; i++)
+	for (int i = 0; i < size_board; i++)
 	{
 		char c[50];
 		GetWindowTextA(X_input_main[i], c, 50);
@@ -361,15 +359,15 @@ bool** solve_main()
 		Mode3::top.push_back(ToVector(c2));
 	}
 	Mode3 a;
-	a = solve_rec_main(a);
+	a = solve_rec(a);
 	if (!a.is_right())
 		throw "Error: no found right solve";
 
-	bool** res = new bool* [size_bord];
-	for (int i = 0; i < size_bord; i++)
+	bool** res = new bool* [size_board];
+	for (int i = 0; i < size_board; i++)
 	{
-		res[i] = new bool[size_bord];
-		for (int j = 0; j < size_bord; j++)
+		res[i] = new bool[size_board];
+		for (int j = 0; j < size_board; j++)
 			res[i][j] = (bool)a[i][j];
 	}
 
@@ -377,7 +375,7 @@ bool** solve_main()
 	return res;
 }
 
-Mode3 solve_rec_main(Mode3 m)
+Mode3 solve_rec(Mode3 m)
 {
 	if (!play)
 		return m;
@@ -390,8 +388,8 @@ Mode3 solve_rec_main(Mode3 m)
 	}
 
 	int index = 0;
-	for (;index < size_bord && m[0][index] != Indefinite; index++);
-	if (index == size_bord)
+	for (;index < size_board && m[0][index] != Indefinite; index++);
+	if (index == size_board)
 		return m;
 
 	vector<int> v(Mode3::top[index].size() + 1);
@@ -400,7 +398,7 @@ Mode3 solve_rec_main(Mode3 m)
 	for (int i = 1; i < v.size() - 1; i++)
 		v[i] = 1;
 
-	int max = size_bord;
+	int max = size_board;
 	if (0 < (int)v.size() - 3) max -= v.size() - 3;
 	for (int i = 0; i < Mode3::top[index].size(); i++)
 		max -= Mode3::top[index][i];
@@ -424,7 +422,7 @@ Mode3 solve_rec_main(Mode3 m)
 		for (int i = 0; i < Mode3::top[index].size(); i++)
 			sum += Mode3::top[index][i];
 
-		if (sum == size_bord)
+		if (sum == size_board)
 		{
 			int index2 = 0;
 			for (int i = 0; i < v.size(); i++)
@@ -441,7 +439,7 @@ Mode3 solve_rec_main(Mode3 m)
 				return m;
 			else if (m.can_be_right())
 			{
-				Mode3 x = solve_rec_main(m);
+				Mode3 x = solve_rec(m);
 				if (x.is_right())
 					return x;
 			}
